9cbb3ffd179c2f06a2c0f5ee4128cb18323c7e3c
Matheus Fernandes <82547552+matheusferrnandes@users.noreply.github.com>
2024-02-25 09:17:10 -0300

    Update Main.java

--- Classes-Carro-EBAC/src/main/java/org/example/Main.java
+++ Classes-Carro-EBAC/src/main/java/org/example/Main.java
@@ -1,30 +1,46 @@
-package org.example;
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
-class Carro{
-    private String marcas;
+// Definição da classe Carro
+class Carro {
+    private String marca;
 
-    public Carro (String marcas){
-        this.marcas = marcas;
-
+    public Carro(String marca) {
+        this.marca = marca;
     }
-    public String getMarcas(){
-        return marcas;
+
+    public String getMarca() {
+        return marca;
     }
 }
 
+// Classe para um tipo específico de carro
+class GM extends Carro {
+    public GM() {
+        super("GM");
+    }
+}
+
+// Classe para outro tipo específico de carro
+class Honda extends Carro {
+    public Honda() {
+        super("Honda");
+    }
+}
+
+// Classe principal
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
+        // Criando a lista de carros
         List<Carro> listaCarros = new ArrayList<>();
 
-        listaCarros.add(new Carro("Toyota"));
-        listaCarros.add(new Carro("Hyundai"));
-        listaCarros.add(new Carro("Audi"));
+        // Instanciando objetos de diferentes tipos de carros e adicionando à lista
+        listaCarros.add(new GM());
+        listaCarros.add(new Honda());
 
+        // Iterando sobre a lista e imprimindo as marcas dos carros
         for (Carro carro : listaCarros) {
-            System.out.println("Marca do Carro: " + carro.getMarcas());
+            System.out.println("Marca do Carro: " + carro.getMarca());
         }
     }
 }
